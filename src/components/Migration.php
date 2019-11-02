42ddbf565fa5ea8961739dcb1c3bcb0568df9dc6
PaulZi <pavel.zimakoff@gmail.com>
2019-11-02 17:01:51 +0300

    fix Migration namespace typo

--- src/components/Migration.php
+++ src/components/Migration.php
@@ -2,7 +2,7 @@
 
 namespace paulzi\cmyii\components;
 
-class Migration extends yii\db\Migration
+class Migration extends \yii\db\Migration
 {
     public $mysqlTableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
 }
